76331087aff8c1b3c9c79dbc99fb5fb2539cc7b0
Gonzalo Orellano <gonzalo.orellano@globallogic.com>
2019-09-18 14:47:46 -0300

    Add configuration, domains, enums, repository and service for auth-service

---
+++ src/main/java/com/authentification/authservice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.authentification.authservice.repository;
+
+import com.authentification.authservice.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
